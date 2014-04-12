5736f9048100e7373b392e616406ea71dc9ff1cb
Josh Anatalio <josephanatalio@joshs-macbook-pro-4.local>
2014-04-12 00:39:25 -0700

    Initial Commit

---
+++ Jackit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Jackit
+//
+//  Created by Josh Anatalio on 4/12/14.
+//  Copyright (c) 2014 JoshAnatalioLAHacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Jackit/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Jackit
+//
+//  Created by Josh Anatalio on 4/12/14.
+//  Copyright (c) 2014 JoshAnatalioLAHacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
